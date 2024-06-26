f5fc6dc25b68058dceeda1b94bd3dcc86905a5a2
nanic <131386211+nanic1@users.noreply.github.com>
2024-06-26 13:42:57 -0300

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-BASE_IMG_PATH = 'data/images/'
-
-def carregar_imagens(path):
-    img = pygame.image.load(BASE_IMG_PATH + path).convert()
-    img.set_colorkey((0, 0, 0))
-    return img
